0179250e2c5bdc533ae1d778e78558da4be04d2c
Michael-crypto-bit <mbrimacombe28@students.stab.org>
2026-09-14 10:45:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,7 @@ import { arrayRemove, serverTimestamp, deleteDoc, where, query, getFirestore, co
 // Your web app's Firebase configuration
 
 // const firebaseConfig = {
-//   apiKey: "AIzaSyDDuxw8usHrNnpBSFVG7QL4VQ0Iqwj3W2g",
-//   authDomain: "softwareengineering-e29f1.firebaseapp.com",
-//   projectId: "softwareengineering-e29f1",
-//   storageBucket: "softwareengineering-e29f1.appspot.com",
-//   messagingSenderId: "232142952313",
-//   appId: "1:232142952313:web:dff5ac81bad1bce13c81a8"
+
 // };
 
 // Initialize Firebase
