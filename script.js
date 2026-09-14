6ac0a6066bacb39b129c3dfa53e06c0934ac4921
Michael-crypto-bit <mbrimacombe28@students.stab.org>
2026-09-14 11:43:06 -0400

    Add files via upload
    added firebase connectivity

--- script.js
+++ script.js
@@ -6,10 +6,43 @@ import { arrayRemove, serverTimestamp, deleteDoc, where, query, getFirestore, co
 
 // Your web app's Firebase configuration
 
-// const firebaseConfig = {
 
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyCinc3NEqDVIxyM4fJ7P8CRo4xMAc1IaXg",
+  authDomain: "makerspace-18269.firebaseapp.com",
+  projectId: "makerspace-18269",
+  storageBucket: "makerspace-18269.firebasestorage.app",
+  messagingSenderId: "343570133102",
+  appId: "1:343570133102:web:5311039f12a0af24e8367f",
+  measurementId: "G-ME346D5CEX"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+export const showItems = async function(){
+ const itemsToDo = document.getElementById("output");
+ itemsToDo.innerHTML = "";
+
+ const incompleteQuery = query(collection(db, "makerspace"));
+ const incompleteSnapshot = await getDocs(incompleteQuery);
+ console.log("firebase successfully read"); 
+ console.log(incompleteQuery);
+ incompleteSnapshot.forEach((item) => {
+  const newPar = document.createElement("label");
+  newPar.innerHTML = item.data().name;
+  console.log(item.data().name);
+  newPar.htmlFor = item.id;
+  const newPar2 = document.createElement("label");
+  if(item.data().category != undefined){
+  newPar2.innerHTML = item.data().category;
+  newPar2.htmlFor = item.id;
+  }
+
+  itemsToDo.appendChild(newPar);
+  itemsToDo.appendChild(document.createElement("br"));
+  itemsToDo.appendChild(newPar2);
+}
+);
+};
