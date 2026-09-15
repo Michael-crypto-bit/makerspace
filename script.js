998ddc1f1bc034d558f6ef5652cbb473ee8a8611
Michael-crypto-bit <mbrimacombe28@students.stab.org>
2026-09-15 14:45:44 -0400

    Add files via upload
    basic firebase connection increased so on the task list now tasks can be partially with optionality for username later, and then the button switches to completing the task, users and progress status is now standardly shown underneath the task

--- script.js
+++ script.js
@@ -25,14 +25,13 @@ export const showItems = async function(){
  const itemsToDo = document.getElementById("output");
  itemsToDo.innerHTML = "";
 
- const incompleteQuery = query(collection(db, "makerspace"));
+ const incompleteQuery = query(collection(db, "makerspace"), where("completed", "==", false));
  const incompleteSnapshot = await getDocs(incompleteQuery);
- console.log("firebase successfully read"); 
- console.log(incompleteQuery);
+ console.warn("firebase successfully read"); 
+ console.info(incompleteQuery);
  incompleteSnapshot.forEach((item) => {
   const newPar = document.createElement("label");
   newPar.innerHTML = item.data().name;
-  console.log(item.data().name);
   newPar.htmlFor = item.id;
   const newPar2 = document.createElement("label");
   if(item.data().category != undefined){
@@ -44,12 +43,61 @@ export const showItems = async function(){
   newPar3.innerHTML = ": ";
   newPar3.htmlFor = item.id;
 
+  const newPar4 = document.createElement("label");
+  const newPar5 = document.createElement("label");
+  if(item.data().inProgress != undefined){
+  newPar4.innerHTML = item.data().inProgress;
+  newPar4.htmlFor = item.id;
+  newPar5.innerHTML = "<br><br>in progress: ";
+  newPar5.htmlFor = item.id;
+  }
+  const checkbox = document.createElement("button");
+  if(item.data().inProgress == false){
+
+  checkbox.innerHTML = "claim";
+  checkbox.onclick = async function(){
+    await updateDoc(doc(db, "makerspace", item.id), {inProgress: true, user: "Michael"});
+    showItems();
+  };
+    }
+  else{
+  checkbox.innerHTML = "complete";
+  checkbox.onclick = async function(){
+    await updateDoc(doc(db, "makerspace", item.id), {completed: true, inProgress: false, user: "Michael"});
+    showItems();
+  };
+  }
+
+  const newPar6 = document.createElement("label");
+  const newPar7 = document.createElement("label");
+  if(item.data().user != undefined){
+  newPar6.innerHTML = item.data().user;
+  newPar6.htmlFor = item.id;
+  newPar7.innerHTML = "<br>user: ";
+  newPar7.htmlFor = item.id;
+  }
+
   itemsToDo.appendChild(newPar2);
   itemsToDo.appendChild(newPar3);
   itemsToDo.appendChild(document.createElement("br"));
   itemsToDo.appendChild(newPar);
+  itemsToDo.appendChild(checkbox);
+  itemsToDo.appendChild(newPar5);
+  itemsToDo.appendChild(newPar4);
+  itemsToDo.appendChild(newPar7);
+  itemsToDo.appendChild(newPar6);
   itemsToDo.appendChild(document.createElement("br"));itemsToDo.appendChild(document.createElement("br"));
   
 }
 );
 };
+export function punch(){
+    console.log("hi");
+    // var status = document.getElementById("statuş").innerText;
+    // if(status == "not punched in"){
+    //     document.getElementById("status").innerText = "punched in";
+    // }
+    // else{
+    //     document.getElementById("status").innerText = "not punched in";
+    // }
+}
