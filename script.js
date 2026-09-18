e0afe29dd07c1dc8813d4d8c78c09005831881b9
QuinnMurphy08 <qmurphy27@students.stab.org>
2026-09-18 11:04:35 -0400

    made the login popup window for admin and students

--- script.js
+++ script.js
@@ -139,3 +139,25 @@ window.onload = function() {
     }
 }
 };
+
+export const showWindow = async function(){
+  const form = document.getElementById('studentLoginForm');
+
+  const currentDisplay = window.getComputedStyle(form).display;
+  if (currentDisplay === 'none' || form.style.display === '') {
+      form.style.display = 'block'; 
+  } else {
+      form.style.display = 'none';
+  }
+};
+
+export const showWindow2 = async function(){
+  const form = document.getElementById('adminLoginForm');
+
+  const currentDisplay = window.getComputedStyle(form).display;
+  if (currentDisplay === 'none' || form.style.display === '') {
+      form.style.display = 'block'; 
+  } else {
+      form.style.display = 'none';
+  }
+};
